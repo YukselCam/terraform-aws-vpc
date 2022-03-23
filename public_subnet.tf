4861968d0730e057b074c665ee86861069a219f7
YukselCam <96206249+YukselCam@users.noreply.github.com>
2022-03-23 15:45:58 -0500

    added public subnets

---
+++ public_subnet.tf
@@ -0,0 +1,15 @@
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets_cidr[0]
+}
+
+
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets_cidr[1]
+}
+
+resource "aws_subnet" "private3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets_cidr[2]
+}
